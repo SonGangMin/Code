addeac530f69f69401022b3f05a4c67f5fadc7a7
SonGangMin <skm0628@naver.com>
2023-05-19 17:49:15 +0900

    0519학원

--- pharmacy/app.js
+++ pharmacy/app.js
@@ -4,9 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-const pharmacyRouter = require("./routes/pharmacy.js");
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
+const indexRouter = require("./routes");
 
 var app = express();
 
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/pharmacy", pharmacyRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -33,15 +33,15 @@ app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
 });
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
 
--- pharmacy/routes/index.js
+++ pharmacy/routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const axios = require("axios");
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/pharmacy", (req, res) => {
+  let api = async () => {
+    let response = null;
+    try {
+      response = await axios.get(
+        "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire",
+        {
+          params: {
+            serviceKey:
+              "pgiyBKl24omVoSPYn1XpeUS4ZKJdwlRx56dSac9Vn0exH9nhJ/48hsbjnGVFZdNtzb0lGSc5x8Vq03wTJBTkUA==",
+            Q0: req.query.Q0,
+            Q1: req.query.Q1,
+            QT: req.query.QT,
+            QN: req.query.QN,
+            ORD: req.query.ORD,
+            pageNo: req.query.pageNo,
+            numOfRows: req.query.numOfRows,
+          },
+        }
+      );
+    } catch (e) {
+      console.error(e);
+    }
+    return response;
+  };
+  api().then((response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(response.data.response.body);
+  });
 });
 
 module.exports = router;
