27560129957469fc8687a0d7786b4d9247c52572
SonGangMin <skm0628@naver.com>
2023-05-28 16:04:36 +0900

    0528 집

--- React/react-practice_ch08/src/App.js
+++ React/react-practice_ch08/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Counter from "./Counter";
+import Info from "./Info";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClickVisible = () => {
+    setVisible(!visible);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClickVisible}>{!visible ? "보이기" : "숨기기"}</button>
+      <Counter />
+      {visible && <Info />}
     </div>
   );
 }
